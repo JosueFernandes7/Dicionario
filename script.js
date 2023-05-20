42b21ae4344d19bbdf555d3af6bf301ad1ed0a8c
Josue Fernandes <josue7mf@hotmail.com>
2023-05-19 21:57:30 -0300

    terminando requisiçoes api

--- script.js
+++ script.js
@@ -1,63 +1,98 @@
+let main = document.querySelector('main')
+let buscaPalavra = document.getElementById('buscar');
+let footer = document.querySelector('footer')
+const container = document.querySelector('.container')
+buscaPalavra.addEventListener('change', carregarDicionario)
+// console.log(palavraBuscada);
 
 
-async function loadTranslation() {
-  texto = JSON.stringify([
-    {
-      "word": "hello",
-      "phonetic": "həˈləʊ",
-      "phonetics": [
-        {
-          "text": "həˈləʊ",
-          "audio": "//ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3"
-        },
-        {
-          "text": "hɛˈləʊ"
-        }
-      ],
-      "origin": "early 19th century: variant of earlier hollo ; related to holla.",
-      "meanings": [
-        {
-          "partOfSpeech": "exclamation",
-          "definitions": [
-            {
-              "definition": "used as a greeting or to begin a phone conversation.",
-              "example": "hello there, Katie!",
-              "synonyms": [],
-              "antonyms": []
-            }
-          ]
-        },
-        {
-          "partOfSpeech": "noun",
-          "definitions": [
-            {
-              "definition": "an utterance of ‘hello’; a greeting.",
-              "example": "she was getting polite nods and hellos from people",
-              "synonyms": [],
-              "antonyms": []
-            }
-          ]
-        },
-        {
-          "partOfSpeech": "verb",
-          "definitions": [
-            {
-              "definition": "say or shout ‘hello’.",
-              "example": "I pressed the phone button and helloed",
-              "synonyms": [],
-              "antonyms": []
-            }
-          ]
-        }
-      ]
+function renderizarSignificado(significados, div) {
+  for ({ partOfSpeech, definitions } of significados) {
+    const significado = document.createElement('div')
+    significado.classList.add('significado')
+    significado.innerHTML +=
+      ` <div class="caixa">
+        <h2>${partOfSpeech}</h2>
+        <div class="line"></div>
+      </div>`
+    const exemplos = document.createElement('div')
+    const ul = document.createElement('ul')
+    exemplos.classList.add('exemplos')
+    exemplos.innerHTML = `<h3>Meaning</h3>`
+    for ({ definition } of definitions) {
+      ul.innerHTML += `<li>${definition}</li>`
     }
-  ])
-  console.log(texto);
-  linguagemNativa = 'pt-BR'
-  linguagemTraducao = 'en-US'
-  const response = await fetch(`https://api.mymemory.translated.net/get?q=${texto}&langpair=${linguagemNativa}|${linguagemTraducao}`)
-  const data = await response.json()
-  console.log(data.responseData.translatedText);
+    exemplos.append(ul)
+    significado.append(exemplos)
+    div.append(significado)
+  }
 }
+function renderizarMain(palavra, fonetica, significados) {
+  main.classList.add('content')
+  main.innerHTML = `
+  <div class="fonetica">
+    <h1 class="result">${palavra}</h1>
+    <span>${fonetica}</span>
+  </div>`
+  const classesDePalavras = document.createElement('div')
+  classesDePalavras.classList.add('classesDePalavras')
+  renderizarSignificado(significados,classesDePalavras)
+  main.append(classesDePalavras)
+  main.innerHTML += `<div class="line"></div>`
 
-loadTranslation()
+}
+function renderizarFooter() {
+
+}
+async function carregarDicionario({ target }) {
+  const word = target.value
+  target.value = ""
+  const url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+  const response = await fetch(`${url}${word}`)
+  const json = await response.json()
+  let data = json[0]
+  if (data) {
+    const linkDicionario = data.sourceUrls[0];
+    const fonetica = data.phonetic;
+    const palavra = data.word
+    const significados = data.meanings.map(({ partOfSpeech, definitions }) => {
+      return { partOfSpeech, definitions }
+    });
+    renderizarMain(palavra, fonetica, significados)
+    // renderizarFooter()
+    // console.log(significados);
+  }
+}
+async function carregarCor() {
+  const url = "http://api.creativehandles.com/getRandomColor"
+  const response = await fetch(url)
+  const json = await response.json()
+  const { color } = json
+  console.log(color);
+  document.documentElement.style.setProperty('--primary-color', color)
+}
+carregarCor()
+
+// async function loadTranslation(texto) {
+//   // traduz de inglês para português
+//   linguagemNativa = 'en-US'
+//   linguagemTraducao = 'pt-BR'
+//   const maxCaracteresPermitidos = 500
+//   let traducao = ""
+//   if(texto.length > maxCaracteresPermitidos) {
+//     let contador = Math.ceil(texto.length / maxCaracteresPermitidos)
+//     for(let i = 0; i < contador; i++) {
+//       textoCortado = texto.slice(i * maxCaracteresPermitidos, (i + 1) * maxCaracteresPermitidos)
+//       traducao += textoCortado
+//       const response = await fetch(`https://api.mymemory.translated.net/get?q=${textoCortado}&langpair=${linguagemNativa}|${linguagemTraducao}`)
+//       const data = await response.json()
+//       traducao += data.responseData.translatedText
+//     }
+//   } else if (texto.length != 0) {
+//     const response = await fetch(`https://api.mymemory.translated.net/get?q=${texto}&langpair=${linguagemNativa}|${linguagemTraducao}`)
+//     const data = await response.json()
+//     traducao += data.responseData.translatedText
+//   }
+//   console.log("\n\n"+traducao);
+//   return traducao ? traducao : null
+// }
